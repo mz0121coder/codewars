ef585ce0382d95e78c1208b8ebfd580bbb1a73e5
Mohammad Zafar <mzcoder7@gmail.com>
2023-02-19 16:28:04 +0000

    better solution

--- 6kyu/encrypt_this.js
+++ 6kyu/encrypt_this.js
@@ -13,23 +13,32 @@ encryptThis("Hello") === "72olle"
 encryptThis("good") === "103doo"
 encryptThis("hello world") === "104olle 119drlo"
 */
-var encryptThis = function (text) {
-	return text.length <= 1
-		? text.charCodeAt(0).toString()
-		: text
-				.split(' ')
-				.map(word =>
-					word.length <= 1
-						? word.charCodeAt(0)
-						: word.length === 2
-						? word.charCodeAt(0) + word.at(-1)
-						: word.charCodeAt(0) +
-						  word.at(-1) +
-						  word.substring(2, word.length - 1) +
-						  word[1]
-				)
-				.join(' ');
-};
+// var encryptThis = function (text) {
+// 	return text.length <= 1
+// 		? text.charCodeAt(0).toString()
+// 		: text
+// 				.split(' ')
+// 				.map(word =>
+// 					word.length <= 1
+// 						? word.charCodeAt(0)
+// 						: word.length === 2
+// 						? word.charCodeAt(0) + word.at(-1)
+// 						: word.charCodeAt(0) +
+// 						  word.at(-1) +
+// 						  word.substring(2, word.length - 1) +
+// 						  word[1]
+// 				)
+// 				.join(' ');
+// };
+const encryptThis = text =>
+	text
+		.split(' ')
+		.map(word =>
+			word
+				.replace(/(^\w)(\w)(\w*)(\w$)/, `$1$4$3$2`)
+				.replace(/^\w/, word.charCodeAt(0))
+		)
+		.join(' ');
 
 console.log(encryptThis('A wise old owl lived in an oak')); // "65 119esi 111dl 111lw 108dvei 105n 97n 111ka"
 console.log(encryptThis('Hello'));
