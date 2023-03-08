8ac16fe4a554f6ef50b0a857e142f2f4925b4acb
Mohammad Zafar <mzcoder7@gmail.com>
2023-03-08 10:54:39 +0000

    description

--- 5kyu/move-zeros-to-end.js
+++ 5kyu/move-zeros-to-end.js
@@ -1,18 +1,23 @@
+/*DESCRIPTION:
+Write an algorithm that takes an array and moves all of the zeros to the end, preserving the order of the other elements.
+
+moveZeros([false,1,0,1,2,0,1,3,"a"]) // returns[false,1,1,2,1,3,"a",0,0]
+*/
 function moveZeros(arr) {
-  let count = 0;
-  const newArr = [];
-  for (let i = 0; i < arr.length; i++) {
-    if (arr[i] !== 0) {
-      // push numbers above 0 to newArr
-      newArr.push(arr[i]);
-    } else {
-      // increment count by 1 for each 0 in arr
-      count++;
-    }
-  }
-  for (let x = count; x > 0; x--) {
-    // push a 0 for every count
-    newArr.push(0);
-  }
-  return newArr;
+	let count = 0;
+	const newArr = [];
+	for (let i = 0; i < arr.length; i++) {
+		if (arr[i] !== 0) {
+			// push numbers above 0 to newArr
+			newArr.push(arr[i]);
+		} else {
+			// increment count by 1 for each 0 in arr
+			count++;
+		}
+	}
+	for (let x = count; x > 0; x--) {
+		// push a 0 for every count
+		newArr.push(0);
+	}
+	return newArr;
 }
--- 5kyu/simple-pig-latin.js
+++ 5kyu/simple-pig-latin.js
@@ -1,13 +1,19 @@
+/*Move the first letter of each word to the end of it, then add "ay" to the end of the word. Leave punctuation marks untouched.
+
+Examples
+pigIt('Pig latin is cool'); // igPay atinlay siay oolcay
+pigIt('Hello world !');     // elloHay orldway !
+*/
 function pigIt(str) {
-  const arr = [];
-  const words = str.split(" ");
-  for (let i = 0; i < words.length; i++) {
-    let w = words[i];
-    if (w.match(/[a-z]/i)) {
-      arr.push(`${w.substring(1)}${w[0]}ay`);
-    } else {
-      arr.push(w);
-    }
-  }
-  return arr.join(" ");
+	const arr = [];
+	const words = str.split(' ');
+	for (let i = 0; i < words.length; i++) {
+		let w = words[i];
+		if (w.match(/[a-z]/i)) {
+			arr.push(`${w.substring(1)}${w[0]}ay`);
+		} else {
+			arr.push(w);
+		}
+	}
+	return arr.join(' ');
 }
