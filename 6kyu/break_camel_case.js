af682fa22ff01e28efcfa5848c443331e950bdca
Mohammad Zafar <mzcoder7@gmail.com>
2023-03-09 14:02:16 +0000

    description

--- 6kyu/break_camel_case.js
+++ 6kyu/break_camel_case.js
@@ -1,11 +1,19 @@
+/*DESCRIPTION:
+Complete the solution so that the function will break up camel casing, using a space between words.
+
+Example
+"camelCasing"  =>  "camel Casing"
+"identifier"   =>  "identifier"
+""             =>  ""*/
+
 function solution(string) {
-  const letters = string.split('');
-  for (let i = 1; i < letters.length; i++) {
-    if (letters[i].match(/[A-Z]/)) {
-      letters[i] = ` ${letters[i].match(/[A-Z]/)}`;
-    }
-  }
-  return letters.join('');
+	const letters = string.split('');
+	for (let i = 1; i < letters.length; i++) {
+		if (letters[i].match(/[A-Z]/)) {
+			letters[i] = ` ${letters[i].match(/[A-Z]/)}`;
+		}
+	}
+	return letters.join('');
 }
 
 console.log(solution('camelCasingTest'));
--- 6kyu/build-a-tower.js
+++ 6kyu/build-a-tower.js
@@ -1,21 +1,43 @@
+/*Build Tower
+Build a pyramid-shaped tower, as an array/list of strings, given a positive integer number of floors. A tower block is represented with "*" character.
+
+For example, a tower with 3 floors looks like this:
+
+[
+  "  *  ",
+  " *** ", 
+  "*****"
+]
+And a tower with 6 floors looks like this:
+
+[
+  "     *     ", 
+  "    ***    ", 
+  "   *****   ", 
+  "  *******  ", 
+  " ********* ", 
+  "***********"
+]
+*/
+
 function towerBuilder(nFloors) {
-  const tower = [];
-  let count = -1;
-  // spacing on each side, increase by 1 for each floor
-  let spaceCount = nFloors;
-  let space = ' ';
-  // decrease bricks by 2 for each floor
-  let brick = '*';
-  for (let i = 0; i < nFloors; i++) {
-    count += 2;
-    spaceCount--;
-    tower.push(
-      `${space.repeat(spaceCount)}${brick.repeat(count)}${space.repeat(
-        spaceCount
-      )}`
-    );
-  }
-  return tower;
+	const tower = [];
+	let count = -1;
+	// spacing on each side, increase by 1 for each floor
+	let spaceCount = nFloors;
+	let space = ' ';
+	// decrease bricks by 2 for each floor
+	let brick = '*';
+	for (let i = 0; i < nFloors; i++) {
+		count += 2;
+		spaceCount--;
+		tower.push(
+			`${space.repeat(spaceCount)}${brick.repeat(count)}${space.repeat(
+				spaceCount
+			)}`
+		);
+	}
+	return tower;
 }
 
 console.log(towerBuilder(6));
