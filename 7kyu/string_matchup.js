89dbef082f028e5206336126d97ab8fc45729cc5
Mohammad Zafar <mzcoder7@gmail.com>
2023-03-27 10:40:33 +0100

    string_matchup.js

---
+++ 7kyu/string_matchup.js
@@ -0,0 +1,36 @@
+/*DESCRIPTION:
+Given two arrays of strings, return the number of times each string of the second array appears in the first array.
+
+Example
+array1 = ['abc', 'abc', 'xyz', 'cde', 'uvw']
+array2 = ['abc', 'cde', 'uap']
+How many times do the elements in array2 appear in array1?
+
+'abc' appears twice in the first array (2)
+'cde' appears only once (1)
+'uap' does not appear in the first array (0)
+Therefore, solve(array1, array2) = [2, 1, 0]
+
+Good luck!
+
+If you like this Kata, please try:
+
+Word values
+
+Non-even substrings
+
+ALGORITHMS*/
+
+function solve(a, b) {
+	const result = [];
+	b.forEach(x => result.push(a.filter(y => y === x).length));
+	return result;
+}
+
+console.log(solve(['abc', 'abc', 'xyz', 'abcd', 'cde'], ['abc', 'cde', 'uap'])); //[(2, 1, 0)]
+
+console.log(solve(['abc', 'xyz', 'abc', 'xyz', 'cde'], ['abc', 'cde', 'xyz'])); //[(2, 1, 2)]
+
+console.log(
+	solve(['quick', 'brown', 'fox', 'is', 'quick'], ['quick', 'abc', 'fox'])
+); //[ (2, 0, 1)]
