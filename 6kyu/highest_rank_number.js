690aacb1a3902c61fd436903f8fe01e0fb655d12
Mohammad Zafar <mzcoder7@gmail.com>
2023-05-14 23:41:07 +0100

    highest_rank_number.js

---
+++ 6kyu/highest_rank_number.js
@@ -0,0 +1,24 @@
+/*DESCRIPTION:
+Complete the method which returns the number which is most frequent in the given input array. If there is a tie for most frequent number, return the largest number among them.
+
+Note: no empty arrays will be given.
+
+Examples
+[12, 10, 8, 12, 7, 6, 4, 10, 12]              -->  12
+[12, 10, 8, 12, 7, 6, 4, 10, 12, 10]          -->  12
+[12, 10, 8, 8, 3, 3, 3, 3, 2, 4, 10, 12, 10]  -->   3
+*/
+
+// sort by frequency
+// get most frequent item
+// if more than one number have highest frequency, return largest number
+
+function highestRank(arr) {
+	return arr.sort(
+		(a, b) => arr.filter(n => n === b).length - arr.filter(n => n === a).length
+	)[0];
+}
+
+console.log(highestRank([12, 10, 8, 12, 7, 6, 4, 10, 12])); // 12
+console.log(highestRank([12, 10, 8, 12, 7, 6, 4, 10, 12, 10])); // 12
+console.log(highestRank([12, 10, 8, 8, 3, 3, 3, 3, 2, 4, 10, 12, 10])); // 3
