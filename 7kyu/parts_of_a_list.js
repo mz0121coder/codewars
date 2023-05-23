905f974dd74cadb3ec4da2b5502cc51107006cc6
Mohammad Zafar <mzcoder7@gmail.com>
2023-05-23 01:22:45 +0100

    refactor

--- 7kyu/parts_of_a_list.js
+++ 7kyu/parts_of_a_list.js
@@ -40,3 +40,12 @@ function partlist(arr) {
 console.log(partlist(['I', 'wish', 'I', "hadn't", 'come'])); //,[["I", "wish I hadn't come"], ["I wish", "I hadn't come"], ["I wish I", "hadn't come"], ["I wish I hadn't", "come"]])
 console.log(partlist(['cdIw', 'tzIy', 'xDu', 'rThG'])); //, [["cdIw", "tzIy xDu rThG"], ["cdIw tzIy", "xDu rThG"], ["cdIw tzIy xDu", "rThG"]])
 console.log(partlist(['vJQ', 'anj', 'mQDq', 'sOZ'])); //, [["vJQ", "anj mQDq sOZ"], ["vJQ anj", "mQDq sOZ"], ["vJQ anj mQDq", "sOZ"]])
+
+function partlist(arr) {
+	const result = [];
+	for (let i = 0; i < arr.length - 1; i++) {
+		const portion = [arr.slice(0, i + 1).join(' '), arr.slice(i + 1).join(' ')];
+		result.push(portion);
+	}
+	return result;
+}
