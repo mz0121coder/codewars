77364a3d948efa75b6b8747d221f29ff2f4d7be6
Mohammad Zafar <mzcoder7@gmail.com>
2023-05-23 01:11:50 +0100

    parts_of_a_list.js

---
+++ 7kyu/parts_of_a_list.js
@@ -0,0 +1,42 @@
+/*
+Write a function partlist that gives all the ways to divide a list (an array) of at least two elements into two non-empty parts.
+
+Each two non empty parts will be in a pair (or an array for languages without tuples or a structin C - C: see Examples test Cases - )
+Each part will be in a string
+Elements of a pair must be in the same order as in the original array.
+Examples of returns in different languages:
+a = ["az", "toto", "picaro", "zone", "kiwi"] -->
+[["az", "toto picaro zone kiwi"], ["az toto", "picaro zone kiwi"], ["az toto picaro", "zone kiwi"], ["az toto picaro zone", "kiwi"]] 
+or
+ a = {"az", "toto", "picaro", "zone", "kiwi"} -->
+{{"az", "toto picaro zone kiwi"}, {"az toto", "picaro zone kiwi"}, {"az toto picaro", "zone kiwi"}, {"az toto picaro zone", "kiwi"}}
+or
+a = ["az", "toto", "picaro", "zone", "kiwi"] -->
+[("az", "toto picaro zone kiwi"), ("az toto", "picaro zone kiwi"), ("az toto picaro", "zone kiwi"), ("az toto picaro zone", "kiwi")]
+or 
+a = [|"az", "toto", "picaro", "zone", "kiwi"|] -->
+[("az", "toto picaro zone kiwi"), ("az toto", "picaro zone kiwi"), ("az toto picaro", "zone kiwi"), ("az toto picaro zone", "kiwi")]
+or
+a = ["az", "toto", "picaro", "zone", "kiwi"] -->
+"(az, toto picaro zone kiwi)(az toto, picaro zone kiwi)(az toto picaro, zone kiwi)(az toto picaro zone, kiwi)"
+Note
+You can see other examples for each language in "Your test cases"
+*/
+
+// loop through each item in arr except last
+// for each item, push an array to result containing
+// 1) arr up to that item, 2) arr after that item
+// both joined with spaces and inside a nested array
+// return result
+
+function partlist(arr) {
+	const result = [];
+	for (let i = 0; i < arr.length - 1; i++) {
+		result.push([arr.slice(0, i + 1).join(' '), arr.slice(i + 1).join(' ')]);
+	}
+	return result;
+}
+
+console.log(partlist(['I', 'wish', 'I', "hadn't", 'come'])); //,[["I", "wish I hadn't come"], ["I wish", "I hadn't come"], ["I wish I", "hadn't come"], ["I wish I hadn't", "come"]])
+console.log(partlist(['cdIw', 'tzIy', 'xDu', 'rThG'])); //, [["cdIw", "tzIy xDu rThG"], ["cdIw tzIy", "xDu rThG"], ["cdIw tzIy xDu", "rThG"]])
+console.log(partlist(['vJQ', 'anj', 'mQDq', 'sOZ'])); //, [["vJQ", "anj mQDq sOZ"], ["vJQ anj", "mQDq sOZ"], ["vJQ anj mQDq", "sOZ"]])
