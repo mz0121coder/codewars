16093b5cf4ae4d38a40ff226c33706d0902a4014
Mohammad Zafar <mzcoder7@gmail.com>
2023-05-26 00:55:26 +0100

    count_of_languages.js

---
+++ 7kyu/count_of_languages.js
@@ -0,0 +1,42 @@
+/*You will be given an array of objects (associative arrays in PHP, table in COBOL) representing data about developers who have signed up to attend the next coding meetup that you are organising.
+
+Your task is to return an object (associative array in PHP, table in COBOL) which includes the count of each coding language represented at the meetup.
+
+For example, given the following input array:
+
+var list1 = [
+  { firstName: 'Noah', lastName: 'M.', country: 'Switzerland', continent: 'Europe', age: 19, language: 'C' },
+  { firstName: 'Anna', lastName: 'R.', country: 'Liechtenstein', continent: 'Europe', age: 52, language: 'JavaScript' },
+  { firstName: 'Ramon', lastName: 'R.', country: 'Paraguay', continent: 'Americas', age: 29, language: 'Ruby' },
+  { firstName: 'George', lastName: 'B.', country: 'England', continent: 'Europe', age: 81, language: 'C' },
+];
+your function should return the following object (associative array in PHP, table in COBOL):
+
+{ C: 2, JavaScript: 1, Ruby: 1 }
+Notes:
+
+The order of the languages in the object does not matter.
+The count value should be a valid number.
+The input array will always be valid and formatted as in the example above.
+
+
+
+
+This kata is part of the Coding Meetup series which includes a number of short and easy to follow katas which have been designed to allow mastering the use of higher-order functions. In JavaScript this includes methods like: forEach, filter, map, reduce, some, every, find, findIndex. Other approaches to solving the katas are of course possible.
+
+Here is the full list of the katas in the Coding Meetup series:*/
+
+// make a result object
+// make a variable (array) for the unique languages
+// for each unique language, find length of items in list with that language, set it as a count variable
+// add a property to result for each language, equal to the count
+
+function countLanguages(list) {
+	const result = {};
+	const languages = [...new Set(list.map(item => item.language))];
+	languages.forEach(el => {
+		const count = list.filter(x => x.language === el).length;
+		result[el] = count;
+	});
+	return result;
+}
